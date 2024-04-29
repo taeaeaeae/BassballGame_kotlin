2c9516ddd284655cb292d73ef1223453a473f5c6
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-04-29 16:43:18 +0900

    🚚 rename : 숫자 체크함수 파일을 새로 생성

--- src/main/kotlin/inline/inputHelper.kt
+++ src/main/kotlin/inline/inputHelper.kt
@@ -4,4 +4,3 @@ inline fun input(prompt: String): String {
     println(prompt)
     return readln()
 }
-inline fun numCheck(num: String): Boolean {return num[0] == num[1] || num[0] == num[2] || num[2] == num[1]}
---
+++ src/main/kotlin/inline/numCheck.kt
@@ -0,0 +1,3 @@
+package inline
+
+inline fun numCheck(num: String): Boolean {return num[0] == num[1] || num[0] == num[2] || num[2] == num[1]}
