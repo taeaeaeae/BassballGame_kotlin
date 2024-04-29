e34c9da12b32c627fa2b9b134793d040840b863a
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-04-29 16:14:32 +0900

    ✨ feat: 랜덤 숫자, 입력값 중복체크

--- src/main/kotlin/Bassball.kt
+++ src/main/kotlin/Bassball.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import inline.numCheck
 import kotlin.random.Random
 
 class Bassball {
@@ -8,7 +9,7 @@ class Bassball {
     //중복숫자없게 이거 추상화 될듯?
     fun check() : String{
         var answerNum : String = Random.nextInt(100, 999).toString()
-        while(answerNum[0] == answerNum[1] || answerNum[0] == answerNum[2] || answerNum[2] == answerNum[1]) {
+        while(numCheck(answerNum)) {
            answerNum = Random.nextInt(100, 999).toString()
         }
         return answerNum
@@ -16,13 +17,21 @@ class Bassball {
 
 
     // 몇볼인지 알려줘
-    fun ballUmpire() {
-
+    fun ballUmpire(answerNum : String, num : String) : Int {
+        var ball = 0
+        for(i in answerNum.indices){
+            if(answerNum.contains(num[i]) && (answerNum[i]==num[i])) ball++
+        }
+        return ball
     }
 
     // 몇 스트라이크야?
-    fun strikeUmpire() {
-
+    fun strikeUmpire(answerNum : String, num : String) : Int {
+        var strike = 0
+        for( i in answerNum.indices){
+            if(answerNum[i] == num[i]) strike++
+        }
+        return strike
     }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,17 @@
 package org.example
 
 import inline.input
+import inline.numCheck
 import kotlin.random.Random
 
 fun main() {
     println("< 게임을 시작합니다 >")
 
+    var num = ""
 
-    val num = input("숫자를 입력하세요")
-
-
-//    val answerNum = "110"
-
-
-
-
-
-
-
-
-
-
+    while (numCheck(num)){
+        input("숫자를 입력하세요")
+    }
 
 
 }
