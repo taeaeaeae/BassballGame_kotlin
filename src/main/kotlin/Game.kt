67c564e13b00ba1623328c15beadddcfaa9dbe26
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-04-30 10:51:09 +0900

    :sparkles: feat : 게임 진행 함수 추가 - 게임 규칙을 알수있게해줌

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,14 +4,16 @@ class Game {
     val bassball = Bassball()
 
     fun guessYou(answer: String, your: String): String {
+        val strike = bassball.strikeUmpire(answer, your)
+        val ball = bassball.ballUmpire(answer, your)
 
-        if (bassball.strikeUmpire(answer, your) == 3) {
+        if (strike== 3) {
+            return "정답입니다."
+        } else if (strike == 0 && ball == 0) {
+            return "Notting"
+        } else {
+            return if(strike == 0) "" else "${strike}스트라이크" + if(ball==0) "" else "${ball}볼"
 
         }
-
-        //if(ball==0) "" else "${ball}볼"
-        //if(strike == 0){""} else if(strike == 3) {"정답입니다!"} else {"${strike}스트라이크"}
-
-        return
     }
 }
