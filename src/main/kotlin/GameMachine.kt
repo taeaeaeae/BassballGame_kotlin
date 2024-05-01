680b868686cbf0c038333e608b5610c46cf6c2a1
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-01 14:20:01 +0900

    :art: style : 줄 정리

--- src/main/kotlin/GameMachine.kt
+++ src/main/kotlin/GameMachine.kt
@@ -5,9 +5,9 @@ class GameMachine {
 
     fun gogo() {
         var toggle = 0
+
         while (toggle != 3) {
             println("환영합니다! 원하시는 번호를 입력해주세요.")
-//            println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
             try{
                 println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
                 toggle = readln()!!.toInt()
